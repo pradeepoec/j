0eb2863e04f295f4a334ed1cffa7acf8e64a358e
pradeepoec <78643119+pradeepoec@users.noreply.github.com>
2021-10-05 00:01:45 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo su -
+yum update -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+systemctl disable firewalld
+systemctl stop firewalld
 cd /var/www/html
 pwd
 echo "<html><h1>Hello qworld C12i3 </h1></html>" >> index.html
